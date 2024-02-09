16772a18f79a67f31975aa1891f7a1e3c66ff359
Patryk Gajek <pgajek94@gmail.com>
2024-02-09 16:24:43 +0100

    useDebounce hook fixed

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 export const useDebounce = <T>(value: T, delay:number = 500) => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
-  useEffect(()=>{
-    const timeout = setTimeout(()=>{
-      setDebouncedValue(debouncedValue);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
     }, delay);
-    console.log('debouncing')
-    return () => clearTimeout(timeout)
-  }, [value, delay])
+
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
 
   return debouncedValue;
 };
