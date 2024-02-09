923ea2c131284b0b896f50955e35cf45f290fd7f
Patryk Gajek <pgajek94@gmail.com>
2024-02-09 13:06:17 +0100

    basic styles and html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="search-wrapper">
+      <label htmlFor="search" className="search">
+        Search Users:
+      </label>
+      <input type="search" id="search" />
+      <div className="user-cards">
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
+        <div className="card">
+          <header className="card-header">My name</header>
+          <div className="card-body">email@emailer.em</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
