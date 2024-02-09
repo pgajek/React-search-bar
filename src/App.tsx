875b89ab66a742b53de18c7123595068bb5aebe1
Patryk Gajek <pgajek94@gmail.com>
2024-02-09 13:29:03 +0100

    data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const getUsers = async () => {
+    const users = await axios.get(url);
+
+    setUsers(users.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <div className="search-wrapper">
       <label htmlFor="search" className="search">
         Search Users:
       </label>
-      <input type="search" id="search" />
+      <input type="search" id="search" value={searchValue} />
       <div className="user-cards">
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
-        <Card name="Name" email="email@emailer.com" />
+        {users.map((user: any, key: number) => (
+          <Card key={key} name={user.name} email={user.email} />
+        ))}
       </div>
     </div>
   );
